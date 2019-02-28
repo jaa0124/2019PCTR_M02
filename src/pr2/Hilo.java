f1baffab989a14887aabf71e05768113c05a36b9
mbg1003 <mbg1003@alu.ubu.es>
2019-02-28 13:47:45 +0100

    Creo clase Hilo con constructor.
    Octavo commit

---
+++ src/pr2/Hilo.java
@@ -0,0 +1,23 @@
+package pr2;
+
+public class Hilo implements Runnable {
+
+	private Ball ball;
+	private Board board;
+	
+	
+	public Hilo(Ball ball, Board board) {
+		
+		this.ball = ball;
+		this.board = board;
+	}
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
