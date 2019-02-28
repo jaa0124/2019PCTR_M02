583a6f302285931aa87ff15d966f953b58803c74
mbg1003 <mbg1003@alu.ubu.es>
2019-02-28 13:33:44 +0100

    Añado update de Ball
    Segundo Commit

--- src/pr2/Ball.java
+++ src/pr2/Ball.java
@@ -1,75 +1,80 @@
+package pr2;
+
 import java.awt.Image;
 import javax.swing.ImageIcon;
-//TODO Transform the code to be used safely in a concurrent context.  
-public class Ball {
-       //TODO  Find an archive named Ball.png 
-	private String Ball = "Ball.png"; 
 
-	private double x,y,dx,dy;
-	private double v,fi;
+//TODO Transform the code to be used safely in a concurrent context.
+public class Ball {
+	// TODO Find an archive named Ball.png
+	private String Ball = "Ball.png";
+	
+	private double x, y, dx, dy;
+	private double v, fi;
 	private Image image;
-	private final int IMG_TAM_X,IMG_TAM_Y;
+	private final int IMG_TAM_X, IMG_TAM_Y;
 
 	public Ball() {
 		ImageIcon ii = new ImageIcon(this.getClass().getResource(Ball));
 		image = ii.getImage();
-		
-		//TODO Depend of image size
-		IMG_TAM_X = 32;
-		IMG_TAM_Y = 32;
 
+		// DONE Depend of image size
+
+		IMG_TAM_X = image.getWidth(null);
+		IMG_TAM_Y = image.getHeight(null);
 		
-		x = Billiards.Width/4-16;
-		y = Billiards.Height/2-16;
+		x = Billiards.Width / 4 - 16;
+		y = Billiards.Height / 2 - 16;
 		v = 5;
-		fi =  Math.random() * Math.PI * 2;
+		fi = Math.random() * Math.PI * 2;
+		assert x <= Board.RIGHTBOARD && x >= Board.LEFTBOARD && y <= Board.TOPBOARD &&  y >= Board.BOTTOMBOARD : "Las bolas no pueden salirse fuera del tablero";
 	}
 
 	public void move() {
-		v = v*Math.exp(-v/1000);
-		dx = v*Math.cos(fi);
-		dy = v*Math.sin(fi);
+		v = v * Math.exp(-v / 1000);
+		dx = v * Math.cos(fi);
+		dy = v * Math.sin(fi);
 		if (Math.abs(dx) < 1 && Math.abs(dy) < 1) {
 			dx = 0;
 			dy = 0;
 		}
-		x += dx;   
+		x += dx;
 		y += dy;
-		
+
 		reflect();
-		
-		//TODO Check postcondition
+
+		assert x <= Board.RIGHTBOARD && x >= Board.LEFTBOARD && y <= Board.TOPBOARD &&  y >= Board.BOTTOMBOARD : "Las bolas no pueden salirse fuera del tablero";
+		// DONE Check postcondition
 	}
 
 	private void reflect() {
-		if (Math.abs(x + IMG_TAM_X - Board.RIGHTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x + IMG_TAM_X - Board.RIGHTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y + IMG_TAM_Y - Board.BOTTOMBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y + IMG_TAM_Y - Board.BOTTOMBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
-		if (Math.abs(x - Board.LEFTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x - Board.LEFTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y - Board.TOPBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y - Board.TOPBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
 	}
 
 	public int getX() {
-		return (int)x;
+		return (int) x;
 	}
-	
+
 	public int getY() {
-		return (int)y;
+		return (int) y;
 	}
-	
+
 	public double getFi() {
 		return fi;
 	}
 
 	public double getdr() {
-		return Math.sqrt(dx*dx+dy*dy);
+		return Math.sqrt(dx * dx + dy * dy);
 	}
 
 	public void setX(double x) {
@@ -85,4 +90,3 @@ public class Ball {
 	}
 
 }
-
