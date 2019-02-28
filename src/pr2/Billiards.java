b90dada47e3508e76b1b96c42c5af64460d7f3d6
mbg1003 <mbg1003@alu.ubu.es>
2019-02-28 13:42:40 +0100

    Añado método actionPerformed() dentro de Billiards
    Sexto Commit

--- src/pr2/Billiards.java
+++ src/pr2/Billiards.java
@@ -68,8 +68,14 @@ public class Billiards extends JFrame {
 	private class StartListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			// TODO Code is executed when start button is pushed
+			// DONE Code is executed when start button is pushed
 			
+			hilos = new Thread[N_BALL];
+			
+			for ( int i = 0; i < N_BALL; i++) {
+				hilos[i] = new Thread( new Hilo(balls[i], board));
+				hilos[i].start();
+			}
 
 		}
 	}
@@ -79,7 +85,6 @@ public class Billiards extends JFrame {
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Code is executed when stop button is pushed
 
-		}
 	}
 	
 
